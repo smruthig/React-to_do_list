6cc0481345a8e2619c0a0f3c10121b093670625e
Smruthi Gowtham <smruthig01@gmail.com>
2021-07-21 08:40:10 +0530

    add task toggle

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { useState } from 'react'
 
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState(
     [
         {
@@ -43,13 +44,16 @@ function App() {
   //Toggle reminder
   const toggleReminder = (id) =>{
     setTasks(tasks.map((task) =>
-      task.id == id ? {...task, reminder: !task.reminder} : task))
+      task.id === id ? {...task, reminder: !task.reminder} : task))
   }
 
   return (
     <div className="container">
-      <Header/>
-      <AddTask onAdd={addTask}/>
+      <Header 
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd = {showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? 
         (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>) : ('No tasks')
       }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,19 @@ import PropTypes from 'prop-types'
 import Button from './Button'
 
 const Header = (props) => {
-    const onClick = () => {
-        console.log('Clicked')
-    }
+    // const onClick = () => {
+    //     props.onAdd()
+    // }
 
     return (
         <div>
             <header className='header'>
                 <h1>{props.title}</h1>
-                <Button text="Add" color="green" onClick={onClick}/>
+                <Button 
+                    text={props.showAdd ? 'Close' : 'Add'} 
+                    color={props.showAdd ? 'red' : 'green'} 
+                    onClick={props.onAdd}
+                />
             </header>
         </div>
     )
